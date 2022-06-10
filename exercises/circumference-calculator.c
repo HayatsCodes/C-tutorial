1ec06fd2aef6817dfb2c2c17de21526c029417af
Hayatudeen Abdulrahman <104792493+religiousCoder@users.noreply.github.com>
2022-06-10 15:57:02 +0100

    Update and rename radius-calculator.c to circumference-calculator.c

---
+++ exercises/circumference-calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* Main - accepts user inputs to calculate the circumference of a circle
+*
+*/
+int main (void)
+{
+  const double PI = 3.14159;
+  double circumference;
+  double radius;
+  
+  printf("Enter radius: ");
+  scanf("%lf", &radius);
+  
+  circumference = 2 * PI * radius;
+  printf("circumference: %lf", circumference);
+  
+  return 0;
+    
