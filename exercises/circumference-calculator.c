740915695612f8e110301d092a1ba1f07d3f0be8
Hayatudeen Abdulrahman <104792493+religiousCoder@users.noreply.github.com>
2022-06-10 17:16:26 +0100

    Update circumference-calculator.c

--- exercises/circumference-calculator.c
+++ exercises/circumference-calculator.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 /**
-* Main - accepts user inputs to calculate the circumference of a circle
+* Main - accepts user inputs to calculate the circumference and area of a circle
 *
 */
 int main (void)
 {
   const double PI = 3.14159;
   double circumference;
+  double area;
   double radius;
   
   printf("Enter radius: ");
   scanf("%lf", &radius);
   
   circumference = 2 * PI * radius;
-  printf("circumference: %lf", circumference);
+  printf("circumference: %lf\n", circumference);
   
+  area = PI * radius * radius;
+  printf("Area: %lf", Area)
   return 0;
 }    
