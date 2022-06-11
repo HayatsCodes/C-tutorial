d51149f41b46550d6a018fb3c742fc6f3f4ed017
Hayatudeen <hayatudeenabdulrahman@gmail.com>
2022-06-11 02:32:41 -0700

    rename cool_projects

---
+++ cool_projects/circumference-calculator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* Main - accepts user inputs to calculate the circumference and area of a circle
+*
+*/
+int main (void)
+{
+  const double PI = 3.14159;
+  double circumference;
+  double area;
+  double radius;
+  
+  printf("Enter radius: ");
+  scanf("%lf", &radius);
+  
+  circumference = 2 * PI * radius;
+  printf("circumference: %lf\n", circumference);
+  
+  area = PI * radius * radius;
+  printf("Area: %lf", area);
+  return 0;
+}    
