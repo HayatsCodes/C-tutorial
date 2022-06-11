552cb9beaf1059c93893a142f48a8e289bc4db70
Hayatudeen Abdulrahman <104792493+religiousCoder@users.noreply.github.com>
2022-06-11 15:25:18 +0100

    Create acceptance-message.c

---
+++ cool_projects/acceptance-message.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+* message - prints message to satandard output
+* 
+* main - calls message function and prints for different users
+* 
+* Return: on success 0.
+*/
+void message(char name[], int duration)
+{
+    printf("\nHello %s. ", name);
+    printf("\nCongratulations! You've been accepted into our %d months program", duration);
+}
+int main(void) 
+{
+   {
+      char name[] = "Fatimah";
+      int duration = 6;
+      message(name, duration);
+   }
+    
+   {
+      char name[] = "Abdulrahman";
+      int duration = 3;
+      message(name, duration);
+   }
+    
+   {
+      char name[] = "Imran";
+      int duration = 9;
+      message(name, duration);
+   }
+   
+   {    
+      char name[] = "Muhammad";
+      int duration = 12;
+      message(name, duration);
+   }
+    
+    return 0;
+}
